958d79d13f787bd964557ed71793a8a44711e398
Brandon Tripp <brandongtripp@gmail.com>
2022-07-22 16:50:26 -0600

    test for AllUsersQuery and UserById query

--- test/payment_server_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PaymentServerWeb.PageControllerTest do
-  use PaymentServerWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
---
+++ test/payment_server_web/schema/queries/user_test.exs
@@ -0,0 +1,66 @@
+defmodule PaymentServerWeb.Schema.Queries.UserTest do 
+  use PaymentServer.DataCase, async: true
+
+  alias PaymentServerWeb.Schema
+  alias PaymentServer.Accounts
+
+
+  @all_user_doc """
+  query AllUsers {
+    users {
+      email
+      id
+      name
+      wallets {
+        currency
+        value
+      }
+    }
+  }
+  """
+
+  describe "@users" do 
+    test "fetches list of users" do 
+      assert {:ok, user} = Accounts.create_user(%{
+        email: "test@test.com",
+        name: "tester1",
+      })
+
+      assert {:ok, user2} = Accounts.create_user(%{
+        email: "test2@test.com",
+        name: "tester2",
+      })
+
+      assert {:ok, %{data: data}} = Absinthe.run(@all_user_doc, Schema)
+
+      assert List.first(data["users"])["id"] === to_string(user.id)
+    end
+  end
+
+  @user_by_id_doc """
+  query UserById($id: ID!) {
+    user(id: $id) {
+      id
+      name
+      email
+    }
+  }
+  """
+
+  describe "@user" do 
+    test "fetch a single user by id" do 
+      assert {:ok, user} = Accounts.create_user(%{
+        email: "test@test.com",
+        name: "tester"
+      })
+
+      assert {:ok, %{data: data}} = Absinthe.run(@user_by_id_doc, Schema,
+        variables: %{
+          "id" => to_string(user.id)
+        }
+      )
+
+      assert data["user"]["id"] === to_string(user.id)
+    end
+  end
+end
