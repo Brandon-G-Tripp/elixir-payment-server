4b400eef4c0a171d2a37d1b3cbf5fe55493f0f45
Brandon Tripp <brandongtripp@gmail.com>
2022-09-19 09:54:25 -0600

    Tests for the gen server functions

--- test/payment_server/exchange_rates_monitor_test.exs
+++ test/payment_server/exchange_rates_monitor_test.exs
@@ -9,16 +9,50 @@ defmodule PaymentServer.ExchangeRatesMonitorTest do
     %{pid: pid}
   end
 
-  describe "&update_rates_state/2" do 
-    test "returns a no reply tuple when called with a valid currency", %{pid: pid} do 
+  describe "&get_exchange_rate/3" do 
+    test "gets exchange rate for specified currency pair", %{pid: _pid} do 
+      ExchangeRatesMonitor.update_rates_state(
+        %{to_currency: "USD", from_currency: "CAD", rate: 1.5}
+      )
+      exchange_rate = ExchangeRatesMonitor.get_exchange_rate(
+        "CAD",
+        "USD"
+      ) 
 
+      assert %{to_currency: "USD", from_currency: "CAD", rate: 1.5} === exchange_rate
+
+      exchange_rate = ExchangeRatesMonitor.get_exchange_rate(
+        "USD",
+        "CAD"
+      ) 
+
+      assert %{to_currency: "CAD", from_currency: "USD", rate: 2.3} === exchange_rate
     end
   end
 
-  describe "&get_exchange_rate/3" do 
+  describe "&update_rates_state/2" do 
+    test "returns a no reply tuple when called with a valid currency", %{pid: _pid} do 
+      ExchangeRatesMonitor.update_rates_state(%{
+        from_currency: "CAD",
+        to_currency: "USD",
+        rate: 2.1
+      })
+
+      exchange_rate = ExchangeRatesMonitor.get_exchange_rate("CAD", "USD")
+
+      assert %{from_currency: "CAD", to_currency: "USD", rate: 2.1} = exchange_rate
+    end
   end
 
+
   describe "&get_all_rates/1" do 
+    test "get list of exchange rates", %{pid: _pid} do 
+      exchange_rate_map = ExchangeRatesMonitor.get_all_rates
+
+      assert Map.has_key?(exchange_rate_map, "CAD/USD")
+      assert Map.has_key?(exchange_rate_map, "USD/CAD")
+
+    end
   end
 
 end
