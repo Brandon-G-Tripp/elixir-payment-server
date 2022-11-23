68c6abea15bb2b99d914001d0e63532bfa3de529
Brandon Tripp <brandongtripp@gmail.com>
2022-11-23 13:24:13 -0700

    remove unnecessary channel macro call

--- lib/payment_server_web/channels/wallet_socket.ex
+++ lib/payment_server_web/channels/wallet_socket.ex
@@ -2,9 +2,6 @@ defmodule PaymentServerWeb.WalletSocket do
   use Phoenix.Socket
   use Absinthe.Phoenix.Socket, schema: PaymentServerWeb.Schema
 
-  channel "wallets", PaymentServerWeb.WalletChannel
-
-
   @impl true
   def connect(_params, socket, _connect_info) do 
     {:ok, socket}
